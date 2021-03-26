daeed28122a97a9502940799e49282fc224465aa
Shakeam <michalzygiel00@gmail.com>
2021-03-26 17:51:43 +0100

    Slider initial

--- script.js
+++ script.js
@@ -1,4 +1,9 @@
 let dropdownButton = document.getElementById("dropdown-button");
+let leftCarouselButton = document.getElementById("left-change");
+let rightCarouselButton = document.getElementById("right-change");
+let leftSlide = document.getElementById("left-slide");
+let centerSlide = document.getElementById("middle-slide");
+let rightSlide = document.getElementById("right-slide");
 
 dropdownButton.addEventListener("click", function(){
     let dropdown = document.getElementById("navbar-dropdown");
@@ -8,4 +13,23 @@ dropdownButton.addEventListener("click", function(){
         dropdown.classList.add("invisible");
     }
     
+});
+
+
+rightCarouselButton.addEventListener("click", function(){
+    let leftSlideContent = document.getElementById("left-slide").innerHTML;
+    let middleSlideContent = document.getElementById("middle-slide").innerHTML;
+    let rightSlideContent = document.getElementById("right-slide").innerHTML;
+    leftSlide.innerHTML = middleSlideContent;
+    centerSlide.innerHTML = rightSlideContent;
+    rightSlide.innerHTML = leftSlideContent;
+});
+
+leftCarouselButton.addEventListener("click", function(){
+    let leftSlideContent = document.getElementById("left-slide").innerHTML;
+    let middleSlideContent = document.getElementById("middle-slide").innerHTML;
+    let rightSlideContent = document.getElementById("right-slide").innerHTML;
+    leftSlide.innerHTML = rightSlideContent;
+    centerSlide.innerHTML = leftSlideContent;
+    rightSlide.innerHTML = middleSlideContent;
 });
